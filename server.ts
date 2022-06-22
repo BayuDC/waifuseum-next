68947accfb13ad7dcf823b9670cd3c61a6b85275
BayuDC <dev.bayudc@gmail.com>
2022-06-22 19:55:07 +0700

    fix: host not set

--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import Fastify, { FastifyInstance } from 'fastify';
 
 const port = process.env.PORT || 8080;
+const host = process.env.NODE_ENV == 'production' ? '0.0.0.0' : '127.0.0.1';
+
 const fastify: FastifyInstance = Fastify();
 
 fastify.register(require('./routes/main'));
 
-fastify.listen({ port: port as number }, (err, addr) => {
+fastify.listen({ port: port as number, host }, (err, addr) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
