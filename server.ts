a0c1c759048bd4ecd1d70fae09857529befc33cc
BayuDC <dev.bayudc@gmail.com>
2022-06-22 20:21:26 +0700

    chore: connect to mongodb

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
 import Fastify, { FastifyInstance } from 'fastify';
+import fastifyMongodb, { FastifyMongodbOptions } from '@fastify/mongodb';
 
 const port = process.env.PORT || 8080;
 const host = process.env.NODE_ENV == 'production' ? '0.0.0.0' : '127.0.0.1';
 
 const fastify: FastifyInstance = Fastify();
+
+fastify.register(require('@fastify/mongodb'), {
+    url: process.env.MONGO_URI,
+    forceClose: true,
+} as FastifyMongodbOptions);
 
 fastify.register(require('./routes/main'));
 
