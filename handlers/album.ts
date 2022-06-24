51c6839185f4996d1a7d3d0022b79eaf9517ff20
BayuDC <dev.bayudc@gmail.com>
2022-06-24 15:05:46 +0700

    refactor: clean up model plugin

--- handlers/album.ts
+++ handlers/album.ts
@@ -3,7 +3,7 @@ import { FastifyRequest, RouteHandlerMethod } from 'fastify';
 export default {
     async index(req: FastifyRequest) {
         const full = (req.query as { full: any }).full != undefined;
-        const albums = await req.server.model.album.find(full);
+        const albums = await req.server.model.find(full);
 
         return { albums };
     },
--- models/album.ts
+++ models/album.ts
@@ -7,55 +7,51 @@ interface AlbumModel {
 
 declare module 'fastify' {
     interface FastifyInstance {
-        model: {
-            album: AlbumModel;
-        };
+        model: AlbumModel;
     }
 }
 
 export default fp(function (fastify: FastifyInstance, options: Object, done: Function) {
     fastify.decorate('model', {
-        album: {
-            async find(full) {
-                return await fastify.mongo.db
-                    ?.collection('albums')
-                    .aggregate([
-                        { $match: { private: false } },
-                        ...(full
-                            ? [
-                                  {
-                                      $lookup: {
-                                          from: 'users',
-                                          localField: 'createdBy',
-                                          foreignField: '_id',
-                                          as: 'createdBy',
-                                          pipeline: [{ $project: { _id: 0, id: '$_id', name: 1 } }],
-                                      },
+        async find(full) {
+            return await fastify.mongo.db
+                ?.collection('albums')
+                .aggregate([
+                    { $match: { private: false } },
+                    ...(full
+                        ? [
+                              {
+                                  $lookup: {
+                                      from: 'users',
+                                      localField: 'createdBy',
+                                      foreignField: '_id',
+                                      as: 'createdBy',
+                                      pipeline: [{ $project: { _id: 0, id: '$_id', name: 1 } }],
                                   },
-                                  { $unwind: '$createdBy' },
-                                  {
-                                      $lookup: {
-                                          from: 'pictures',
-                                          localField: '_id',
-                                          foreignField: 'album',
-                                          as: 'pictures',
-                                          pipeline: [{ $count: 'count' }],
-                                      },
+                              },
+                              { $unwind: '$createdBy' },
+                              {
+                                  $lookup: {
+                                      from: 'pictures',
+                                      localField: '_id',
+                                      foreignField: 'album',
+                                      as: 'pictures',
+                                      pipeline: [{ $count: 'count' }],
                                   },
-                                  { $unwind: '$pictures' },
-                              ]
-                            : []),
-                        {
-                            $project: {
-                                ...{ _id: 0, id: '$_id', name: 1, slug: 1, private: 1, community: 1 },
-                                ...(full ? { createdAt: 1, createdBy: 1, picturesCount: '$pictures.count' } : {}),
-                            },
+                              },
+                              { $unwind: '$pictures' },
+                          ]
+                        : []),
+                    {
+                        $project: {
+                            ...{ _id: 0, id: '$_id', name: 1, slug: 1, private: 1, community: 1 },
+                            ...(full ? { createdAt: 1, createdBy: 1, picturesCount: '$pictures.count' } : {}),
                         },
-                    ])
-                    .toArray();
-            },
-        } as AlbumModel,
-    });
+                    },
+                ])
+                .toArray();
+        },
+    } as AlbumModel);
 
     done();
 });
