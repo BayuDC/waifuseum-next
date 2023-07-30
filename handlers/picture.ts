a898596985d9770dbd13b8b8a91d858ffb1f9481
BayuDC <dev.bayudc@gmail.com>
2023-07-31 01:19:51 +0700

    feat: paginate today pictures

--- handlers/picture.ts
+++ handlers/picture.ts
@@ -2,14 +2,21 @@ import { RouteHandlerMethod } from 'fastify';
 
 import Picture from '../models/picture';
 
+interface PictureQuery {
+    page: number;
+    count: number;
+}
+
 export default {
     async index(req, reply) {
         const now = new Date();
         const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
+        const { page, count } = req.query as PictureQuery;
+
         const pictures = await Picture.find({
             createdAt: { $gte: today },
-        }).paginate(1, 10);
+        }).paginate(page, count);
 
         return { pictures };
     },
--- routes/picture.ts
+++ routes/picture.ts
@@ -1,9 +1,11 @@
 import { FastifyPluginCallback, RouteShorthandOptionsWithHandler } from 'fastify';
 
 import handler from '../handlers/picture';
+import schema from '../schemas/picture';
 
 export default (function (fastify, options, done) {
     fastify.get('/pictures/today', {
+        schema: schema.index,
         handler: handler.index,
     } as RouteShorthandOptionsWithHandler);
 
